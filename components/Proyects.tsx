df34b9d68b87537ccbe87c811932316a3f91e616
L1ghtnessTL <shadowhunter07213@gmail.com>
2023-05-04 20:56:23 -0500

    add some pages

--- components/Proyects.tsx
+++ components/Proyects.tsx
@@ -22,17 +22,17 @@ export const Proyects = () => {
                     <span className='text-gray-99 text-sm text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam molestiae fuga asperiores.</span>
                 </div>
 
-                <div className='my-10  p-20 w-full flex bg-[#ECE8E9] rounded-xl'>
+                <div className='my-10  p-10 md:p-20 w-full flex bg-[#ECE8E9] rounded-xl'>
 
-                    <div className='w-1/2 xls:w-full  '>
+                    <div className='md:w-1/2 :w-full  '>
                         <h3 className='text-2xl text-gray-300 mb-4 font-semibold xls:text-xl xls:font-bold '>Rick & Morty {width}</h3>
-                        <span className='text-gray-99 text-sm '> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad id provident ut quaerat, eveniet distinctio praesentium? Deserunt tempora autem itaque officiis dignissimos earum adipisci.</span>
+                        <span className='text-gray-99 text-sm  '> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad id provident ut quaerat, eveniet distinctio praesentium? Deserunt tempora autem itaque officiis dignissimos earum adipisci.</span>
                         <button className='block mt-10 font-semibold text-teal-200 cursor-pointer px-6 lg:px-8 py-3 lg:py-4 border border-solid border-3 bg-cyan-100 rounded-full text-gray-200 hover:bg-cyan-200 border-none transition-all'>
                             Visit now
                         </button>
                     </div>
 
-                    <div className='w-1/2 flex xls:hidden md:block'>
+                    <div className='w-1/2 flex hidden md:block'>
                         <Image className="ml-20" src="/media/rick-and-morty.png" alt="rick" width={width <= 795 ? 150 : 250} height={width <= 795 ? 150 : 250} />
                     </div>
                 </div>
---
+++ pages/Proyects.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Proyects() {
+    return (
+        <main>Proyects</main>
+    )
+}
